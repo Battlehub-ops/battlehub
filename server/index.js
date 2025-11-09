0035f756e83b14223b382afed25823422495331d
Battlehub-ops <trevor96mukisa@gmail.com>
2025-11-09 19:53:53 +0300

    Fix CORS preflight handler

--- server/index.js
+++ server/index.js
@@ -15,7 +15,12 @@ app.use(cors({
 }));
 
 // allow CORS preflight on all routes
-app.options('*', cors());
+app.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-admin-key');
+  res.sendStatus(204);
+});
 
 const FRONTEND_URL = process.env.BASE_URL || 'https://battlehub-frontend.vercel.app';
 app.use(cors({ origin: [FRONTEND_URL] }));
